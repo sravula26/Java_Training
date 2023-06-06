0d5603e49a897fd938cb8336bad6c3c6009b4f1c
sravula26 <ravulasharathreddy@gmail.com>
2023-06-06 15:24:46 -0400

    hello

---
+++ src/first/Employee_Details.java
@@ -0,0 +1,21 @@
+package first;
+import java.util.Scanner;
+public class Employee_Details {
+
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		//System.out.println("Hello");
+		
+		String empName;
+		
+		int age;
+		Double Salary;
+		int id;
+		String project;
+		
+		
+				
+		
+	}
+}
---
+++ src/first/student.java
@@ -0,0 +1,12 @@
+package first;
+import java.util.Scanner;
+import java.lang.*;
+public class student {
+	int id;
+	String name;
+	int rollno;
+	String gender;
+	
+	
+
+}
